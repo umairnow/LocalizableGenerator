6b364f58ae8ad45fe9e832c5fa73b74d659b1766
Umair Aamir <umair@myworkout.no>
2017-11-26 21:21:50 +0100

    Initial Commit

---
+++ SwiftLocGen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLocGen
+//
+//  Created by Umair Aamir on 11/26/17.
+//  Copyright © 2017 Fine8 AS. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
